a96e7acef99141f52faa2556bbaebfb572a14852
magnesyljuasen <msylju@gmail.com>
2023-10-27 11:22:58 +0200

    fiks

--- app2.py
+++ app2.py
@@ -234,7 +234,7 @@ def show_pydeck_map(df, last_value):
         "ColumnLayer",
         data=df,
         get_position=["lng", "lat"],
-        get_elevation="verdi_fornybar_energi",
+        get_elevation="verdi_fornybar_energi_number",
         elevation_scale=0.0001,
         radius=10,
         get_fill_color=[0, color, 0],
@@ -242,7 +242,8 @@ def show_pydeck_map(df, last_value):
         #auto_highlight=True,
     )
     tooltip = {
-        "html": "<b> Gimse skole </b><br>Fornybar energi er nå verdt {verdi_fornybar_energi} kr.<br>- Grunnvarme: {verdi_grunnvarme} kr<br> - Solceller: {verdi_sol} kr.",
+        #"html": "<b> Gimse skole </b><br>Fornybar energi er nå verdt {verdi_fornybar_energi} kr.<br>- Grunnvarme: {verdi_grunnvarme} kr<br> - Solceller: {verdi_sol} kr.",
+        "html": "<b> Gimse skole </b><br>Fornybar energi<br>er nå verdt {verdi_fornybar_energi} kr.",
         "style": {
         "backgroundColor": "white",
         "color": "black"
@@ -263,32 +264,61 @@ def show_pydeck_map(df, last_value):
 
 if __name__ == "__main__":
     streamlit_settings()
-    #st_autorefresh(interval=1 * 15 * 1000, key="dataframerefresh")
     #--
+    properate = Properate(ID = "AH_7224_Gammelbakkan_15")
+    #--
+    time_series_gshp = "TS_7224_Gammelbakkan_15+Common=100.001-OC001-BB001"
+    time_series_solar = "TS_7224_Gammelbakkan_15+GB15=471.001-OE001-OE001-OF001"
+    df_gshp_value, metadata_gshp_value = properate.get_timeseries(time_series_gshp)
+    df_solar_value, metadata_solar_value = properate.get_timeseries(time_series_solar)
+    df_merged_values = pd.merge(df_solar_value, df_gshp_value, left_index=True, right_index=True, how='outer')
+    df_merged_values.fillna(0, inplace=True)
+    df_merged_values["Verdi fornybar energi"] = df_merged_values[time_series_gshp] + df_merged_values[time_series_solar]
+    df_merged_values = df_merged_values.drop_duplicates(subset=['Verdi fornybar energi'])
+    df_merged_values['Verdi fornybar energi akkumulert'] = df_merged_values["Verdi fornybar energi"].cumsum()
+    maximum_value_accumulated = int(np.max(df_merged_values['Verdi fornybar energi akkumulert'])*1.1) 
+
+    df_merged_values['Timestamp'] = pd.to_datetime(df_merged_values.index)
+    latest_timestamp = df_merged_values['Timestamp'].max()
+    twenty_four_hours_ago = latest_timestamp - pd.DateOffset(hours=24)
+    df_merged_values_last_24 = df_merged_values[df_merged_values['Timestamp'] >= twenty_four_hours_ago]
+    #--
+    time_series_1 = "TS_7224_Gammelbakkan_15+GB15=320.002-LV001-OE001_Geo_heat_energy-OE001"
+    df_gshp_production_1, metadata_gshp_production_1 = properate.get_timeseries(time_series_1)
+    time_series_2 = "TS_7224_Gammelbakkan_15+GB15=320.003-LV001-OE001_Geo_heat_energy-OE001"
+    df_gshp_production_2, metadata_gshp_production_1 = properate.get_timeseries(time_series_2)
+
+    df_gshp_production = pd.merge(df_gshp_production_1, df_gshp_production_2, left_index=True, right_index=True, how='outer')
+    df_gshp_production["Produsert grunnvarme"] = df_gshp_production[time_series_1] + df_gshp_production[time_series_2]
+    
+    time_series_solar_production = "TS_7224_Gammelbakkan_15+GB15=471.001-OE001-OE001"
+    df_solar_production, metadata_solar = properate.get_timeseries(time_series_solar_production)
+
+    df_merged_production = pd.merge(df_gshp_production, df_solar_production, left_index=True, right_index=True, how='outer')
+    df_merged_production.fillna(0, inplace=True)
+    df_merged_production["Produsert fornybar energi"] = df_merged_production["Produsert grunnvarme"] + df_merged_production[time_series_solar_production]
+    df_merged_production['Produsert fornybar energi akkumulert'] = df_merged_production["Produsert fornybar energi"].cumsum()
+    maximum_value_produced = int(np.max(df_merged_production['Produsert fornybar energi akkumulert'])*1.1)
+
+    df_merged_production['Timestamp'] = pd.to_datetime(df_merged_production.index)
+    latest_timestamp = df_merged_production['Timestamp'].max()
+    twenty_four_hours_ago = latest_timestamp - pd.DateOffset(hours=24)
+    df_merged_production_last_24 = df_merged_production[df_merged_production['Timestamp'] >= twenty_four_hours_ago]
+    #--
+
     col1, col2 = st.columns(2)
-    with col1:
-        properate = Properate(ID = "AH_7224_Gammelbakkan_15")
-        time_series_gshp = "TS_7224_Gammelbakkan_15+Common=100.001-OC001-BB001"
-        df_gshp, metadata = properate.get_timeseries(time_series_gshp)
-        
-        time_series_solar = "TS_7224_Gammelbakkan_15+GB15=471.001-OE001-OE001-OF001"
-        df_solar, metadata = properate.get_timeseries(time_series_solar)
-        
-        last_value = int(float(df_gshp.to_numpy()[-1]) + float(df_solar.to_numpy()[-1]))
-        total_value = int(np.sum(df_gshp.to_numpy()) + np.sum(df_solar.to_numpy()))
-        solar_value = int(np.sum(df_solar.to_numpy()))
-        gshp_value = int(np.sum(df_gshp.to_numpy()))
-        
+    with col1:        
         df = pd.DataFrame({
             "id" : ["Gimse skole"],
             "lat" : [63.286463],
             "lng" : [10.263912],
-            "verdi_fornybar_energi" : [total_value],
-            "verdi_fornybar_momentan" : [last_value],
-            "verdi_sol" : [solar_value],
-            "verdi_grunnvarme" : [gshp_value]
+            "verdi_fornybar_energi" : [f"{int(df_merged_values['Verdi fornybar energi akkumulert'][-1]):,}".replace(",", " ")],
+            "verdi_fornybar_energi_number" : [int(df_merged_values['Verdi fornybar energi akkumulert'][-1])],
+            "verdi_fornybar_momentan" : [f"{int(np.sum(df_merged_values_last_24['Verdi fornybar energi'])):,}".replace(",", " ")],
+#            "verdi_sol" : [int(df_merged_values["Verdi fornybar energi"][-1])],
+#            "verdi_grunnvarme" : [int(df_merged_values["Verdi fornybar energi"][-1])]
         })
-        r = show_pydeck_map(df = df, last_value = last_value)
+        r = show_pydeck_map(df = df, last_value = 100)
         
         st.pydeck_chart(pydeck_obj = r, use_container_width = True)
         #--
@@ -296,18 +326,7 @@ if __name__ == "__main__":
         with st.expander("Gimse skole", expanded = True):
             c1, c2 = st.columns(2)
             with c1:
-                #--
-                time_series_gshp = "TS_7224_Gammelbakkan_15+Common=100.001-OC001-BB001"
-                time_series_solar = "TS_7224_Gammelbakkan_15+GB15=471.001-OE001-OE001-OF001"
-                df_gshp_value, metadata_gshp_value = properate.get_timeseries(time_series_gshp)
-                df_solar_value, metadata_solar_value = properate.get_timeseries(time_series_solar)
-                df_merged_values = pd.merge(df_solar_value, df_gshp_value, left_index=True, right_index=True, how='outer')
-                df_merged_values.fillna(0, inplace=True)
-                df_merged_values["Verdi fornybar energi"] = df_merged_values[time_series_gshp] + df_merged_values[time_series_solar]
-                df_merged_values['Verdi fornybar energi akkumulert'] = df_merged_values["Verdi fornybar energi"].cumsum()
-                maximum_value_accumulated = int(np.max(df_merged_values['Verdi fornybar energi akkumulert'])*1.1) 
-
-                fig = px.line(df_merged_values, x=df_merged_values.index, y=df_merged_values["Verdi fornybar energi akkumulert"], title=f'Besparelse<br>• Totalt: {int(df_merged_values["Verdi fornybar energi akkumulert"][-1]):,} kr<br>• Siste 24 timer: {int(np.sum(df_merged_values["Verdi fornybar energi"][-288:-1])):,} kr'.replace(",", " "))
+                fig = px.line(df_merged_values, x=df_merged_values.index, y=df_merged_values["Verdi fornybar energi akkumulert"], title=f'Besparelse<br>• Totalt: {int(df_merged_values["Verdi fornybar energi akkumulert"][-1]):,} kr<br>• Siste 24 timer: {int(np.sum(df_merged_values_last_24["Verdi fornybar energi"])):,} kr'.replace(",", " "))
                 maximum_value = int(np.max(df_merged_values["Verdi fornybar energi akkumulert"])*1.1)
                 fig.update_traces(line_color='#1d3c34', line_width=2)
                 fig.update_xaxes(
@@ -327,25 +346,7 @@ if __name__ == "__main__":
                 fig.update_layout(separators="* .*")
                 st.plotly_chart(fig, use_container_width=True)
             with c2:
-                #--
-                time_series_1 = "TS_7224_Gammelbakkan_15+GB15=320.002-LV001-OE001_Geo_heat_energy-OE001"
-                df_gshp_production_1, metadata_gshp_production_1 = properate.get_timeseries(time_series_1)
-                time_series_2 = "TS_7224_Gammelbakkan_15+GB15=320.003-LV001-OE001_Geo_heat_energy-OE001"
-                df_gshp_production_2, metadata_gshp_production_1 = properate.get_timeseries(time_series_2)
-
-                df_gshp_production = pd.merge(df_gshp_production_1, df_gshp_production_2, left_index=True, right_index=True, how='outer')
-                df_gshp_production["Produsert grunnvarme"] = df_gshp_production[time_series_1] + df_gshp_production[time_series_2]
-                
-                time_series_solar_production = "TS_7224_Gammelbakkan_15+GB15=471.001-OE001-OE001"
-                df_solar_production, metadata_solar = properate.get_timeseries(time_series_solar_production)
-
-                df_merged_production = pd.merge(df_gshp_production, df_solar_production, left_index=True, right_index=True, how='outer')
-                df_merged_production.fillna(0, inplace=True)
-                df_merged_production["Produsert fornybar energi"] = df_merged_production["Produsert grunnvarme"] + df_merged_production[time_series_solar_production]
-                df_merged_production['Produsert fornybar energi akkumulert'] = df_merged_production["Produsert fornybar energi"].cumsum()
-                maximum_value_produced = int(np.max(df_merged_production['Produsert fornybar energi akkumulert'])*1.1)
-
-                fig = px.line(df_merged_production, x=df_merged_production.index, y=df_merged_production['Produsert fornybar energi akkumulert'], title=f'Produsert fornybar energi<br>• Totalt: {int(df_merged_production["Produsert fornybar energi akkumulert"][-1]):,} kWh<br>• Siste 24 timer: {int(np.sum(df_merged_production["Produsert fornybar energi"][-24:-1])):,} kWh'.replace(",", " "))
+                fig = px.line(df_merged_production, x=df_merged_production.index, y=df_merged_production['Produsert fornybar energi akkumulert'], title=f'Produsert fornybar energi<br>• Totalt: {int(df_merged_production["Produsert fornybar energi akkumulert"][-1]):,} kWh<br>• Siste 24 timer: {int(np.sum(df_merged_production_last_24["Produsert fornybar energi"])):,} kWh'.replace(",", " "))
                 fig.update_traces(line_color='#1d3c34', line_width=2)
                 fig.update_xaxes(
                     title_text='',
@@ -364,6 +365,7 @@ if __name__ == "__main__":
                 fig.update_layout(separators="* .*")
                 st.plotly_chart(fig, use_container_width=True) 
 
+    #
     time_loop()
     st.experimental_rerun()
 
